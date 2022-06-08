848509b8bdfe46086b48bf02a91ebb1d427ccaa3
umahanty <106217275+umahanty@users.noreply.github.com>
2022-06-08 20:48:48 +0530

    day5selectionprocasequestion

---
+++ selectionprocase2.sh
@@ -0,0 +1,14 @@
+echo "Enter a number"
+read n
+
+case $n in
+1) echo "sunday" ;;
+2)echo "monday" ;;
+3) echo "Tuesday" ;;
+4) echo "Wednesday" ;;
+5) echo "Thursday" ;;
+6) echo "Friday" ;;
+7) echo "Saturday" ;;
+*) echo "enter value between 1 to 7" ;;
+
+esac
